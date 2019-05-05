92d0c2dd5705a20fff72c54c65582e27c0b0782c
Ben LaVerriere <benlaverriere@gmail.com>
2019-05-05 19:27:52 -0400

    Add Markdown parsing for `note` field

--- make_placards.py
+++ make_placards.py
@@ -5,31 +5,47 @@ import json
 from io import open
 
 import jinja2
+import mistune
 
-PARSER = argparse.ArgumentParser(
-    description="Generate printable placards based on JSON metadata."
-)
-PARSER.add_argument("file", metavar="FILE", help="the input file to read")
-PARSER.add_argument(
-    "-o",
-    "--output",
-    dest="outfile",
-    default="placards_generated.html",
-    help="optional output filename",
-)
-PARSER.add_argument(
-    "-t",
-    "--template",
-    dest="template",
-    default="placards_template.html",
-    help="optional template filename",
-)
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description="Generate printable placards based on JSON metadata."
+    )
+    parser.add_argument("file", metavar="FILE", help="the input file to read")
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="outfile",
+        default="placards_generated.html",
+        help="optional output filename",
+    )
+    parser.add_argument(
+        "-t",
+        "--template",
+        dest="template",
+        default="placards_template.html",
+        help="optional template filename",
+    )
+    return parser
 
+
+def markdownformat(value):
+    return MARKDOWN(str(value))
+
+
+def create_jinja_env():
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader("."),
+        autoescape=jinja2.select_autoescape(["html"])
+    )
+    env.filters['markdown'] = markdownformat
+    return env
+
+
+MARKDOWN = mistune.Markdown()
+PARSER = create_parser()
 ARGS = PARSER.parse_args()
-
-ENV = jinja2.Environment(
-    loader=jinja2.FileSystemLoader("."), autoescape=jinja2.select_autoescape(["html"])
-)
+ENV = create_jinja_env()
 TEMPLATE = ENV.get_template("placards_template.html")
 
 with open(ARGS.file) as file:
