43e268e114b55ab37b795334b67710df8ab495ce
Ben LaVerriere <ben.laverriere@bookbub.com>
2021-02-06 14:41:33 -0500

    Read template from file; write results to file

--- index.js
+++ index.js
@@ -6,24 +6,28 @@ async function csv2json(inputCSV) {
   return await csv().fromFile(inputCSV);
 }
 
-function json2html(inputJSON) {
-  console.log(inputJSON);
-  const template = '{{@each(it) => value, index}}\
-    <div>{{value.title}} {{value.year}}</div>\
-  {{/each}}';
+function json2html(inputJSON, templatePath) {
+  const template = fs.readFileSync(templatePath, { encoding: 'utf8', flag: 'r' });
   return sqrl.render(template, inputJSON);
 }
 
-async function render(inputCSV) {
+async function render(inputCSV, templatePath, outputPath) {
   const json = await csv2json(inputCSV);
-  const html = json2html(json);
-  console.log(html);
+  const html = json2html(json, templatePath);
+  fs.writeFileSync(outputPath, html);
+  console.log(`Written to ${outputPath}`);
 }
 
 const argv = process.argv.slice(2);
 const inputCSV = argv[0];
+const template = argv[1];
+const outputPath = argv[2];
 if (inputCSV == null || inputCSV.length == 0) {
-  console.error('Please specify the input CSV file to render:\n  generate FILE');
+  console.error('Please specify a CSV file to render:\n  generate INPUT_FILE TEMPLATE_FILE OUTPUT_FILE');
+} else if (template == null || template.length == 0) {
+  console.error('Please specify a template HTML file to use:\n  generate INPUT_FILE TEMPLATE_FILE OUTPUT_FILE');
+} else if (outputPath == null || outputPath.length == 0) {
+  console.error('Please specify an output HTML file:\n  generate INPUT_FILE TEMPLATE_FILE OUTPUT_FILE');
 } else {
-  render(inputCSV);
+  render(inputCSV, template, outputPath);
 }
